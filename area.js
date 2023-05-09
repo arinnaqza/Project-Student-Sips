2eaba7efb0e72f405dbc7ec4379f086b424d2c36
Winnie <winnieeslyn@gmail.com>
2023-05-09 22:42:08 +0900

    Changed the layout of Area page and made the transition smoother

--- area.js
+++ area.js
@@ -1,34 +1,31 @@
-filterObjects("all");
+const cafeBtns = document.querySelectorAll('.cafe-btn');
+const cafeLists = document.querySelectorAll('.cafe-list');
 
-function filterObjects(c){
-    var x, i;
-    x = document.getElementsByClassName("box");
-    if (c == "all") c="";
-    for(i=0; i<x.length; i++){
-        removeClass(x[i], "show");
-        if(x[i].className.indexOf(c) > -1) addClass(x[i],"show")
-    } 
+let activeBtn = "wangsimni";
+
+showCafeArea(activeBtn);
+
+cafeBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        resetActiveBtn();
+        showCafeArea(btn.id);
+        btn.classList.add('active-btn');
+    });
+});
+
+function resetActiveBtn(){
+    cafeBtns.forEach((btn) => {
+        btn.classList.remove('active-btn');
+    });
 }
 
-function addClass(element, name){
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for(i=0; i < arr2.length; i++){
-        if(arr1.indexOf(arr2[i]) == -1){
-            element.className += " " + arr2[i];
+function showCafeArea(newCafeBtn){
+    activeBtn = newCafeBtn;
+    cafeLists.forEach((item) => {
+        if(item.classList.contains(activeBtn)){
+            item.style.display = "grid";
+        } else {
+            item.style.display = "none";
         }
-    }
-}
-
-function removeClass(element, name){
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for(i=0; i < arr2.length; i++){
-        while(arr1.indexOf(arr2[i]) > -1){
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
-    }
-    element.className = arr1.join(" ");
+    });
 }
